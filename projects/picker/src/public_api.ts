6a3e49f14508f601ebd5ee08aa942db2f8bd26ad
NetWin <netwin@codedcomplexity.de>
2024-11-02 23:02:39 +0100

    docs(*): cleanup public api

--- projects/picker/src/public_api.ts
+++ projects/picker/src/public_api.ts
@@ -1,20 +1,26 @@
+// Adapters
 export * from './lib/date-time/adapter/date-time-adapter.class';
 export * from './lib/date-time/adapter/date-time-format.class';
 export * from './lib/date-time/adapter/native-date-time-adapter.class';
+export * from './lib/date-time/adapter/native-date-time.module';
 export * from './lib/date-time/adapter/unix-timestamp-adapter/unix-timestamp-date-time-adapter.class';
 export * from './lib/date-time/adapter/unix-timestamp-adapter/unix-timestamp-date-time-format.class';
+
+// Services / Providers
+export * from './lib/date-time/date-time-picker-intl.service';
 export * from './lib/date-time/options-provider';
 
-export * from './lib/date-time/adapter/native-date-time.module';
-export * from './lib/date-time/date-time-picker-intl.service';
-export * from './lib/date-time/date-time.module';
-
+// Components
 export * from './lib/date-time/calendar-body.component';
 export * from './lib/date-time/calendar-month-view.component';
 export * from './lib/date-time/calendar-multi-year-view.component';
 export * from './lib/date-time/calendar-year-view.component';
 export * from './lib/date-time/calendar.component';
-export * from './lib/date-time/date-time-inline.component';
 export * from './lib/date-time/timer.component';
 
+// Main exports
+export * from './lib/date-time/date-time-inline.component';
+export * from './lib/date-time/date-time.module';
+
+// Types
 export * from './lib/types';
